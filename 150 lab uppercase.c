4f78fad4502cb753f3e32c4adaab64a6988d3b21
dhyey1-lab <dhyey2707@gmail.com>
2025-09-30 16:44:29 +0530

    Add files via upload

---
+++ 150 lab uppercase.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "hi my name is dhyey ";
+    printf("The length of the string is: %s\n", strupr(str1));
+    return 0;
+}
---
+++ 150 lower case converstion.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[] = "HI MY NAME IS DHYEY";
+    printf("The length of the string is: %s\n", strlwr(str1));
+    return 0;
+}
