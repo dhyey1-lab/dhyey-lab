e61039d3b127576bcc3f10606d118252aa4665d1
Dhyey Agaja <dhyey2707@gmail.com>
2025-11-11 17:04:07 +0530

    Add files via upload

---
+++ 150 lab last.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+
+struct students {
+    char name[20];
+    char course[20];
+    char major_sub[20];
+    char minor_sub[20];
+    int roll;
+}S1,S2,S3,S4,S5,S6,S7,S8,S9,S10;
+
+void print_student(struct students s) {
+    printf("Name        : %s\n", s.name);
+    printf("Course      : %s\n", s.course);
+    printf("Major Sub   : %s\n", s.major_sub);
+    printf("Minor Sub   : %s\n", s.minor_sub);
+    printf("Roll Number : %d\n", s.roll);
+}
+
+int main() {
+    struct students stu;
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+    printf("Enter student name: ");
+    scanf("%19s", stu.name);
+
+    printf("Enter course name: ");
+    scanf("%19s", stu.course);
+
+    printf("Enter major subject name: ");
+    scanf("%19s", stu.major_sub);
+
+    printf("Enter minor subject name: ");
+    scanf("%19s", stu.minor_sub);
+
+    printf("Enter roll number: ");
+    scanf("%d", &stu.roll);
+
+    printf("\n--- Student Details ---\n");
+    print_student(stu);
+
+
+    return 0;
+}
+
+
+
